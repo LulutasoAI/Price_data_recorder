b3d34cdfded50a48073b4f953c12178073d6a32a
LulutasoAI <hinety3@gmail.com>
2022-11-25 14:07:35 +0900

    core function 1 implemented

--- Datetime_wrapper.py
+++ Datetime_wrapper.py
@@ -3,3 +3,6 @@ from datetime import datetime
 class Datetime_wrapper():
     def __init__(self) -> None:
         pass
+    
+    def get_current_time(self):
+        return datetime.now()
--- Price_data_recorder.py
+++ Price_data_recorder.py
@@ -2,10 +2,15 @@ import pandas as pd
 import numpy as np 
 import datetime 
 from Price_data_handler import Price_data_handler
+from Datetime_wrapper import Datetime_wrapper
+import schedule
+import time
+
 class Price_data_recorder():
     
     def __init__(self) -> None:
         self.price_data_handler = Price_data_handler()
+        self.datetime_wrapper = Datetime_wrapper()
         self.symbol = "^N225"
     
     def change_target(self,target_symbol):
@@ -17,9 +22,21 @@ class Price_data_recorder():
     def get_latest_price(self):
         self.price_data_handler.get_current_price(self.symbol)
 
+    def get_data(self):
+        current_price = self.get_latest_price()
+        current_time = self.datetime_wrapper.get_current_time()
+        return current_price, current_time
+    def record_price(self):
+        current_price,current_time = self.get_data()
+        print(current_time)
+        
     def start_recording(self):
         self.get_latest_price()
-        pass 
+        schedule.every(1).seconds.do(self.record_price)
+        while True:
+            schedule.run_pending()
+            time.sleep(0.1)
+            
 
 
 if __name__ == "__main__":
